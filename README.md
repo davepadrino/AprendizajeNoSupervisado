4e94437b93c4220e48d7a288f3da02bca0dc0144
David Padrino <dpadrino@4geeks.com.ve>
2016-04-02 23:07:53 -0430

    tareas terminadas, OJO con algunas, terminar a.csv, empezar el resto de archivos

--- README.md
+++ README.md
@@ -3,4 +3,8 @@
 
 ## David Padrino
 
+# Tasks
+## Ver si se muestran todos los valores probados o solo los elegidos como los mejores algoritmos
+## Pulir a.csv
+## Empezar con los otros archivos
 
--- Tarea3.R
+++ Tarea3.R
@@ -1,6 +1,8 @@
 # install.packages("clue")
 library("stringr")
 library("caret")
+library("clue")
+library("cluster")
  
 ##### ***** a.csv ***** #####
 
@@ -9,81 +11,118 @@ a <- read.csv("a.csv")
 names(a)[1] <- "x"
 names(a)[2] <- "y"
 names(a)[3] <- "class"
-dim(a)
-names(a)
-str(a)
-summary(a)
-hist(a$x)
-plot(a$x, a$y, col=1:3) 
+# dim(a)
+# names(a)
+# str(a)
+# summary(a)
+# hist(a$x)
+# plot(a$x, a$y, col=1:3) 
 
 ## K means
 a.kmeans <- kmeans(a[,1:2], centers = 3)
-plot(a$x, a$y, col= a.kmeans$cluster)
-points(a.kmeans$centers[, c("x", "y")],
-       col=1:3,
-       pch = 19,
-       cex = 2)
-table(a.kmeans$cluster, a$class)
+a.kmeans.confusion.Matrix <- table(a.kmeans$cluster, a$class)
+a.kmeans.accuracy.CM <- sum(diag(a.kmeans.confusion.Matrix))/sum(a.kmeans.confusion.Matrix)
+
 
 ## Partitioning Around Medioids (PAM)
 a.pam <- pam(a[,1:2], 3)
-plot(a$x, a$y, col = a.pam$clustering)
-points(a.pam$medoids,
-       col=1:3,
-       pch = 18,
-       cex = 2)
+a.pam.confusion.Matrix <- table(a.pam$clustering, a$class)
+a.pam.accuracy.CM <- sum(diag(a.pam.confusion.Matrix))/sum(a.pam.confusion.Matrix)
 
 #### Comparison of kmean's centroids vs pam's medioids
-a.kmeans$centers
-a.pam$medoids # Medioids are part of the DataSet
+if (a.kmeans.accuracy.CM >= a.pam.accuracy.CM){
+  kmeans.vs.pam <- a.kmeans.accuracy.CM
+  kmeans.vs.pam.choose <- c("kmeans")
+}else{
+  kmeans.vs.pam <- a.pam.accuracy.CM
+  kmeans.vs.pam.choose <- c("pam")
+}
+
+
+# Medioids are part of the DataSet, centroids aren't necessarily
 
 
 
 ## Hcluster
-hclust_methods <- c("ward.D", "single", "complete", "average", "mcquitty", "median", "centroid", "ward.D2")
-dist_methods <- c("euclidean", "maximum", "manhattan", "canberra", "binary", "minkowski")
+a.hclust_methods <- c("ward.D", "single", "complete", "average", "mcquitty", "median", "centroid", "ward.D2")
+a.dist_methods <- c("euclidean", "maximum", "manhattan", "binary", "minkowski")
 
 a.num <- a # a copy of the dataframe
 a.num$class <-NULL # Delete class column
-a.num <- as.matrix(a.num) # convert into a matri
+a.num <- as.matrix(a.num) # convert into a matrix
 
 # Calculating each distance method vs each hclust method 
-better.accuracy <- 0
-for (i in 1:length(dist_methods)){
-  for (j in 1:length(hclust_methods)){
-    a.dist.mat <- dist(a.num, method = dist_methods[i]) # distance matrix
-    a.cluster <- hclust(a.dist.mat, method = hclust_methods[j]) # apply method
+a.hclust.better.accuracy <- 0
+for (i in 1:length(a.dist_methods)){
+  for (j in 1:length(a.hclust_methods)){
+    a.dist.mat <- dist(a.num, method = a.dist_methods[i]) # distance matrix
+    a.cluster <- hclust(a.dist.mat, method = a.hclust_methods[j]) # apply method
     ct <- cutree(a.cluster, k =3) # k to generate 3 clusters
-    confusion.Matrix <- table(as.factor(a$class), as.factor(ct))
-    accuracy.CM <- sum(diag(confusion.Matrix))/sum(confusion.Matrix)
-    if (accuracy.CM > better.accuracy){
-      better.methods <- c(dist_methods[i], hclust_methods[j])
-      better.accuracy <- accuracy.CM
+    a.hclust.confusion.Matrix <- table(as.factor(a$class), as.factor(ct))
+    a.hclust.accuracy.CM <- sum(diag(a.hclust.confusion.Matrix))/sum(a.hclust.confusion.Matrix)
+    if (a.hclust.accuracy.CM > a.hclust.better.accuracy){
+      a.better.methods <- c(a.dist_methods[i], a.hclust_methods[j])
+      a.hclust.better.accuracy <- a.hclust.accuracy.CM
     }
+  }
+}
+
+# Comparison hclust vs kmeans.vs.pam
+if (a.hclust.better.accuracy > kmeans.vs.pam){
+  final.cluster <- a.hclust.better.accuracy
+  a.dist.mat <- dist(a.num, method = a.better.methods[1]) # distance matrix
+  a.cluster <- hclust(a.dist.mat, method = a.better.methods[2]) # apply method
+  a.ct <- cutree(a.cluster, k =3) # k to generate 3 clusters
+  plot(a.cluster) # dendrogram
+  rect.hclust(a.cluster, k = 3, border = c("cyan"))
+  plot(a$x, a$y, col= a.ct, main = "HCluster")
+  dendrogram <- as.dendrogram(a.cluster)
+  corte <- cut(dendrogram, h=20)$upper
+  plot(corte)
+}else{
+  final.cluster <- kmeans.vs.pam
+  if (kmeans.vs.pam.choose[1] == 'pam'){
+    plot(a$x, a$y, col = a.pam$clustering, main = "PAM")
+    points(a.pam$medoids,
+           col=1:3,
+           pch = 18,
+           cex = 3)
+  }else{
+    plot(a$x, a$y, col= a.kmeans$cluster, main = "K-means")
+    points(a.kmeans$centers[, c("x", "y")],
+           col=1:3,
+           pch = 19,
+           cex = 3)
   }
 }
 
 
 
-plot(a.cluster) # dendrogram
-plot(a$x, a$y, col= ct)
 
 
-ct1 <- cutree(a.cluster, h =15) # h para la altura
-plot(a$x, a$y, col = ct1)
-table(a$class, ct1)
 
 
-dendrogram <- as.dendrogram(a.cluster)
-plot(dendrogram)
-rect.hclust(a.cluster, k = 3, border = c("cyan"))
+
+
+
+
+
+
+
+
+
+
+
+#ct1 <- cutree(a.cluster, h =15) # h para la altura
+#plot(a$x, a$y, col = ct1)
+#table(a$class, ct1)
+
+
+#dendrogram <- as.dendrogram(a.cluster)
+#plot(dendrogram)
 # Cortar el dendrograma por encima
-corte <- cut(dendrogram, h=1)$upper
-plot(corte)
 
 
-
-## Confussion Matrix
 
 
 ##### ***** moon.csv ***** #####
@@ -95,7 +134,7 @@ names(moon)
 str(moon)
 summary(moon)
 
-
+# dist_methods <- c("euclidean", "maximum", "manhattan", "canberra", "binary", "minkowski")
 ##### ***** h.csv ***** #####
 
 ## Exploratory Analysis
