55e675331cc9ae5e9a791fc23bffc684bb337c2b
David Padrino <dpadrino@4geeks.com.ve>
2016-04-04 23:12:28 -0430

    a y moon listos, trabajando en h.csv

--- README.md
+++ README.md
@@ -4,7 +4,14 @@
 ## David Padrino
 
 # Tasks
-## Ver si se muestran todos los valores probados o solo los elegidos como los mejores algoritmos
-## Pulir a.csv
-## Empezar con los otros archivos
+## Check "points" in h.csv
+## Try to randomize the points and no clusterize inmediatly
+
+
+Se utilizó el SO Ubuntu 14.04.
+Para el paquete Rcmdr puede dar cierto error con un elemento X11, para ello se recomienda ejecutar en el terminal de Ubuntu
+
+sudo apt-get build-dep r-cran-rgl
+
+
 
--- Tarea3.R
+++ Tarea3.R
@@ -1,10 +1,17 @@
 # install.packages("clue")
+# install.packages("Rcmdr")
 library("stringr")
 library("caret")
 library("clue")
 library("cluster")
+library("rgl")
+library("Rcmdr")
+
+# First set the location directory
+
+
  
-##### ***** a.csv ***** #####
+############################ ***** a.csv ***** #######################################
 
 ## Exploratory Analysis
 a <- read.csv("a.csv")
@@ -19,10 +26,13 @@ names(a)[3] <- "class"
 # plot(a$x, a$y, col=1:3) 
 
 ## K means
-a.kmeans <- kmeans(a[,1:2], centers = 3)
-a.kmeans.confusion.Matrix <- table(a.kmeans$cluster, a$class)
-a.kmeans.accuracy.CM <- sum(diag(a.kmeans.confusion.Matrix))/sum(a.kmeans.confusion.Matrix)
-
+a.kmeans.accuracy.CM <- 0
+for (i in 1:5){
+  a.kmeans <- kmeans(a[,c("x", "y")], centers = 3)
+  a.kmeans.confusion.Matrix <- table(a.kmeans$cluster, a$class)
+  a.kmeans.accuracy.CM <- c(a.kmeans.accuracy.CM, sum(diag(a.kmeans.confusion.Matrix))/sum(a.kmeans.confusion.Matrix))
+}
+a.kmeans.accuracy.CM <- max(a.kmeans.accuracy.CM)
 
 ## Partitioning Around Medioids (PAM)
 a.pam <- pam(a[,1:2], 3)
@@ -30,6 +40,7 @@ a.pam.confusion.Matrix <- table(a.pam$clustering, a$class)
 a.pam.accuracy.CM <- sum(diag(a.pam.confusion.Matrix))/sum(a.pam.confusion.Matrix)
 
 #### Comparison of kmean's centroids vs pam's medioids
+kmeans.vs.pam <- 0
 if (a.kmeans.accuracy.CM >= a.pam.accuracy.CM){
   kmeans.vs.pam <- a.kmeans.accuracy.CM
   kmeans.vs.pam.choose <- c("kmeans")
@@ -41,11 +52,9 @@ if (a.kmeans.accuracy.CM >= a.pam.accuracy.CM){
 
 # Medioids are part of the DataSet, centroids aren't necessarily
 
-
-
 ## Hcluster
-a.hclust_methods <- c("ward.D", "single", "complete", "average", "mcquitty", "median", "centroid", "ward.D2")
-a.dist_methods <- c("euclidean", "maximum", "manhattan", "binary", "minkowski")
+hclust_methods <- c("ward.D", "single", "complete", "average", "mcquitty", "median", "centroid", "ward.D2")
+dist_methods <- c("euclidean", "maximum", "manhattan", "binary", "minkowski")
 
 a.num <- a # a copy of the dataframe
 a.num$class <-NULL # Delete class column
@@ -53,15 +62,15 @@ a.num <- as.matrix(a.num) # convert into a matrix
 
 # Calculating each distance method vs each hclust method 
 a.hclust.better.accuracy <- 0
-for (i in 1:length(a.dist_methods)){
-  for (j in 1:length(a.hclust_methods)){
-    a.dist.mat <- dist(a.num, method = a.dist_methods[i]) # distance matrix
-    a.cluster <- hclust(a.dist.mat, method = a.hclust_methods[j]) # apply method
-    ct <- cutree(a.cluster, k =3) # k to generate 3 clusters
-    a.hclust.confusion.Matrix <- table(as.factor(a$class), as.factor(ct))
+for (i in 1:length(dist_methods)){
+  for (j in 1:length(hclust_methods)){
+    a.dist.mat <- dist(a.num, method = dist_methods[i]) # distance matrix
+    a.cluster <- hclust(a.dist.mat, method = hclust_methods[j]) # apply method
+    a.ct <- cutree(a.cluster, k =3) # k to generate 3 clusters
+    a.hclust.confusion.Matrix <- table(as.factor(a$class), as.factor(a.ct))
     a.hclust.accuracy.CM <- sum(diag(a.hclust.confusion.Matrix))/sum(a.hclust.confusion.Matrix)
     if (a.hclust.accuracy.CM > a.hclust.better.accuracy){
-      a.better.methods <- c(a.dist_methods[i], a.hclust_methods[j])
+      a.better <- c(dist_methods[i], hclust_methods[j])
       a.hclust.better.accuracy <- a.hclust.accuracy.CM
     }
   }
@@ -70,14 +79,14 @@ for (i in 1:length(a.dist_methods)){
 # Comparison hclust vs kmeans.vs.pam
 if (a.hclust.better.accuracy > kmeans.vs.pam){
   final.cluster <- a.hclust.better.accuracy
-  a.dist.mat <- dist(a.num, method = a.better.methods[1]) # distance matrix
-  a.cluster <- hclust(a.dist.mat, method = a.better.methods[2]) # apply method
+  a.dist.mat <- dist(a.num, method = a.better[1]) # distance matrix
+  a.cluster <- hclust(a.dist.mat, method = a.better[2]) # apply method
   a.ct <- cutree(a.cluster, k =3) # k to generate 3 clusters
   plot(a.cluster) # dendrogram
   rect.hclust(a.cluster, k = 3, border = c("cyan"))
   plot(a$x, a$y, col= a.ct, main = "HCluster")
   dendrogram <- as.dendrogram(a.cluster)
-  corte <- cut(dendrogram, h=20)$upper
+  corte <- cut(dendrogram, h=20)$upper # $upper to get useful information instead a forest
   plot(corte)
 }else{
   final.cluster <- kmeans.vs.pam
@@ -98,6 +107,7 @@ if (a.hclust.better.accuracy > kmeans.vs.pam){
 
 
 
+################################### END a.csv ##############################################
 
 
 
@@ -107,53 +117,281 @@ if (a.hclust.better.accuracy > kmeans.vs.pam){
 
 
 
-
-
-
-
-
-
-#ct1 <- cutree(a.cluster, h =15) # h para la altura
-#plot(a$x, a$y, col = ct1)
-#table(a$class, ct1)
-
-
-#dendrogram <- as.dendrogram(a.cluster)
-#plot(dendrogram)
-# Cortar el dendrograma por encima
-
-
-
-
-##### ***** moon.csv ***** #####
+############################## ***** moon.csv *****  ######################################
 
 ## Exploratory Analysis
 moon <- read.csv("moon.csv")
+names(moon)[1] <- "x"
+names(moon)[2] <- "y"
+names(moon)[3] <- "class"
 dim(moon)
 names(moon)
 str(moon)
 summary(moon)
+plot(moon$x, moon$y, col=1:2) 
 
-# dist_methods <- c("euclidean", "maximum", "manhattan", "canberra", "binary", "minkowski")
-##### ***** h.csv ***** #####
+
+## K means
+moon.kmeans.accuracy.CM <- 0
+for (i in 1:5){
+  moon.kmeans <- kmeans(moon[,c("x", "y")], centers = 2)
+  moon.kmeans.CM <- table(moon.kmeans$cluster, moon$class)
+  moon.kmeans.accuracy.CM <- c(moon.kmeans.accuracy.CM, sum(diag(moon.kmeans.CM))/sum(moon.kmeans.CM))
+}
+moon.kmeans.accuracy.CM <- max(moon.kmeans.accuracy.CM)
+
+### ***** plot kmeans 
+plot(moon$x, moon$y, col= moon.kmeans$cluster, main = "K-means")
+points(moon.kmeans$centers[, c("x", "y")],
+       col=1:2,
+       pch = 19,
+       cex = 3)
+###
+
+
+## Partitioning Around Medioids (PAM)
+moon.pam <- pam(moon[,1:2], 2)
+moon.pam.CM <- table(moon.pam$clustering, moon$class)
+moon.pam.accuracy.CM <- sum(diag(moon.pam.CM))/sum(moon.pam.CM)
+
+### ***** plot PAM
+plot(moon$x, moon$y, col = moon.pam$clustering, main = "PAM")
+points(moon.pam$medoids,
+       col=1:2,
+       pch = 18,
+       cex = 3)
+###
+
+
+#### Comparison of kmean's centroids vs pam's medioids
+kmeans.vs.pam <- 0
+if (moon.kmeans.accuracy.CM >= moon.pam.accuracy.CM){
+  kmeans.vs.pam <- moon.kmeans.accuracy.CM
+  kmeans.vs.pam.choose <- c("kmeans")
+}else{
+  kmeans.vs.pam <- moon.pam.accuracy.CM
+  kmeans.vs.pam.choose <- c("pam")
+}
+
+
+# Medioids are part of the DataSet, centroids aren't necessarily
+
+## Hcluster
+moon.num <- moon # a copy of the dataframe
+moon.num$class <-NULL # Delete class column
+moon.num <- as.matrix(moon.num) # convert into a matrix
+
+# Calculating each distance method vs each hclust method 
+moon.hclust.better.accuracy <- 0
+for (i in 1:length(dist_methods)){
+  for (j in 1:length(hclust_methods)){
+    moon.dist.mat <- dist(moon.num, method = dist_methods[i]) # distance matrix
+    moon.cluster <- hclust(moon.dist.mat, method = hclust_methods[j]) # apply method
+    moon.ct <- cutree(moon.cluster, k =2) # k to generate 3 clusters
+    moon.hclust.CM <- table(as.factor(moon$class), as.factor(moon.ct))
+    moon.hclust.accuracy.CM <- sum(diag(moon.hclust.CM))/sum(moon.hclust.CM)
+    if (moon.hclust.accuracy.CM > moon.hclust.better.accuracy){
+      moon.better <- c(dist_methods[i], hclust_methods[j])
+      #moon.cluster.better <- moon.cluster
+      #moon.ct.better <- moon.ct
+      moon.hclust.better.accuracy <- moon.hclust.accuracy.CM
+    }
+  }
+}
+
+#### ****** plot HCLUST
+plot(moon.cluster.better) # dendrogram
+rect.hclust(moon.cluster.better, k = 2, border = c("cyan"))
+plot(moon$x, moon$y, col= moon.ct.better, main = "HCluster")
+dendrogram <- as.dendrogram(moon.cluster.better)
+corte <- cut(dendrogram, h=20)$upper # $upper to get useful information instead a forest
+plot(corte)
+######
+
+
+
+
+
+# Comparison hclust vs kmeans.vs.pam
+if (moon.hclust.better.accuracy > kmeans.vs.pam){
+  final.cluster <- moon.hclust.better.accuracy
+  moon.dist.mat <- dist(moon.num, method = moon.better[1]) # distance matrix
+  moon.cluster <- hclust(moon.dist.mat, method = moon.better[2]) # apply method
+  moon.ct <- cutree(moon.cluster, k =2) # k to generate 3 clusters
+  plot(moon.cluster) # dendrogram
+  rect.hclust(moon.cluster, k = 2, border = c("cyan"))
+  plot(moon$x, moon$y, col= moon.ct, main = "HCluster")
+  dendrogram <- as.dendrogram(moon.cluster)
+  corte <- cut(dendrogram, h=20)$upper # $upper to get useful information instead a forest
+  plot(corte)
+}else{
+  final.cluster <- kmeans.vs.pam
+  if (kmeans.vs.pam.choose[1] == 'pam'){
+    plot(moon$x, moon$y, col = moon.pam$clustering, main = "PAM")
+    points(moon.pam$medoids,
+           col=1:2,
+           pch = 18,
+           cex = 3)
+  }else{
+    plot(moon$x, moon$y, col= moon.kmeans$cluster, main = "K-means")
+    points(moon.kmeans$centers[, c("x", "y")],
+           col=1:2,
+           pch = 19,
+           cex = 3)
+  }
+}
+
+
+
+################################### END moon.csv ##############################################
+
+
+
+
+
+
+
+
+
+############################### ***** h.csv ***** ###############################
+h.clase = function(numero){
+  # Selecting 3 clusters
+  if(numero < 7.0)
+    return(1)
+  else if(numero < 11.0)
+    return(2)
+  else
+    return(3)
+}
+
 
 ## Exploratory Analysis
 h <- read.csv("h.csv")
+names(h)[1] <- "x"
+names(h)[2] <- "y"
+names(h)[3] <- "z"
+names(h)[4] <- "class"
+for (i in 1:length(h$class)){
+  h$class[i] <- h.clase(h$class[i])
+}
 dim(h)
 names(h)
 str(h)
 summary(h)
+plot3d(h$x, h$y, h$z, col = h$class)
+
 
 ## K means
+h.kmeans.accuracy.CM <- 0
+for (i in 1:5){
+  h.kmeans <- kmeans(h[,c("x", "y", "z")], centers = 3)
+  h.kmeans.CM <- table(h.kmeans$cluster, h$class)
+  # In this case, as a list, must use as.numeric and unlist functions
+  h.kmeans.accuracy.CM <- c(h.kmeans.accuracy.CM, sum(diag(as.numeric(unlist(h.kmeans))))/sum(as.numeric(unlist(h.kmeans))))
+}
+h.kmeans.accuracy.CM <- max(h.kmeans.accuracy.CM)
+
+### ***** plot kmeans 
+plot3d(h$x, h$y, h$z, col = h$class, main = "K-means")
+# plot3d.points
+points(h.kmeans$centers[, c("x", "y", "z")],
+       col=1:3,
+       pch = 19,
+       cex = 3)
+###
+
+
+## Partitioning Around Medioids (PAM)
+moon.pam <- pam(moon[,1:2], 2)
+moon.pam.CM <- table(moon.pam$clustering, moon$class)
+moon.pam.accuracy.CM <- sum(diag(moon.pam.CM))/sum(moon.pam.CM)
+
+### ***** plot PAM
+plot(moon$x, moon$y, col = moon.pam$clustering, main = "PAM")
+points(moon.pam$medoids,
+       col=1:2,
+       pch = 18,
+       cex = 3)
+###
+
+
+#### Comparison of kmean's centroids vs pam's medioids
+kmeans.vs.pam <- 0
+if (moon.kmeans.accuracy.CM >= moon.pam.accuracy.CM){
+  kmeans.vs.pam <- moon.kmeans.accuracy.CM
+  kmeans.vs.pam.choose <- c("kmeans")
+}else{
+  kmeans.vs.pam <- moon.pam.accuracy.CM
+  kmeans.vs.pam.choose <- c("pam")
+}
+
+
+# Medioids are part of the DataSet, centroids aren't necessarily
 
 ## Hcluster
+moon.num <- moon # a copy of the dataframe
+moon.num$class <-NULL # Delete class column
+moon.num <- as.matrix(moon.num) # convert into a matrix
 
-## Confussion Matrix
-accuracy <- sum(diag(confusionMatrix.PART))/sum(confusionMatrix.PART)
+# Calculating each distance method vs each hclust method 
+moon.hclust.better.accuracy <- 0
+for (i in 1:length(dist_methods)){
+  for (j in 1:length(hclust_methods)){
+    moon.dist.mat <- dist(moon.num, method = dist_methods[i]) # distance matrix
+    moon.cluster <- hclust(moon.dist.mat, method = hclust_methods[j]) # apply method
+    moon.ct <- cutree(moon.cluster, k =2) # k to generate 3 clusters
+    moon.hclust.CM <- table(as.factor(moon$class), as.factor(moon.ct))
+    moon.hclust.accuracy.CM <- sum(diag(moon.hclust.CM))/sum(moon.hclust.CM)
+    if (moon.hclust.accuracy.CM > moon.hclust.better.accuracy){
+      moon.better <- c(dist_methods[i], hclust_methods[j])
+      #moon.cluster.better <- moon.cluster
+      #moon.ct.better <- moon.ct
+      moon.hclust.better.accuracy <- moon.hclust.accuracy.CM
+    }
+  }
+}
+
+#### ****** plot HCLUST
+plot(moon.cluster.better) # dendrogram
+rect.hclust(moon.cluster.better, k = 2, border = c("cyan"))
+plot(moon$x, moon$y, col= moon.ct.better, main = "HCluster")
+dendrogram <- as.dendrogram(moon.cluster.better)
+corte <- cut(dendrogram, h=20)$upper # $upper to get useful information instead a forest
+plot(corte)
+######
 
 
 
 
+
+# Comparison hclust vs kmeans.vs.pam
+if (moon.hclust.better.accuracy > kmeans.vs.pam){
+  final.cluster <- moon.hclust.better.accuracy
+  moon.dist.mat <- dist(moon.num, method = moon.better[1]) # distance matrix
+  moon.cluster <- hclust(moon.dist.mat, method = moon.better[2]) # apply method
+  moon.ct <- cutree(moon.cluster, k =2) # k to generate 3 clusters
+  plot(moon.cluster) # dendrogram
+  rect.hclust(moon.cluster, k = 2, border = c("cyan"))
+  plot(moon$x, moon$y, col= moon.ct, main = "HCluster")
+  dendrogram <- as.dendrogram(moon.cluster)
+  corte <- cut(dendrogram, h=20)$upper # $upper to get useful information instead a forest
+  plot(corte)
+}else{
+  final.cluster <- kmeans.vs.pam
+  if (kmeans.vs.pam.choose[1] == 'pam'){
+    plot(moon$x, moon$y, col = moon.pam$clustering, main = "PAM")
+    points(moon.pam$medoids,
+           col=1:2,
+           pch = 18,
+           cex = 3)
+  }else{
+    plot(moon$x, moon$y, col= moon.kmeans$cluster, main = "K-means")
+    points(moon.kmeans$centers[, c("x", "y")],
+           col=1:2,
+           pch = 19,
+           cex = 3)
+  }
+}
 
 ##### ***** s.csv ***** #####
 
